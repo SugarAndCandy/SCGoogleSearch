a7a787a168e48a906a22196a1805b44a087531e2
Maxim Kolesnik <makleso6@gmail.com>
2015-12-17 10:46:19 +0300

    Initial Commit

---
+++ SCGoogleSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCGoogleSearch
+//
+//  Created by Maxim Kolesnik on 17.12.15.
+//  Copyright © 2015 Sugar and Candy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
